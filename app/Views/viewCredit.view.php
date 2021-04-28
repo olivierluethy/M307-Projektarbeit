ae8672d6d4be062dcdb0935be2325259732d0641
Olivier <olivier.luethy@gmx.net>
2021-04-28 14:48:17 +0200

    Changed Table-Design on viewCredit.view.php

--- app/Views/viewCredit.view.php
+++ app/Views/viewCredit.view.php
@@ -5,86 +5,74 @@
     <title>Verleih Ansicht</title>
 </head>
 <body>
-    <!-- <fieldset>
-        <legend>Personal Daten</legend>
-        <label for="Name">Name:</label>
-        <input type="text" name="name"><br><br>
-
-        <label for="email">Email:</label>
-        <input type="text" name="email"><br><br>
-
-        <label for="telefon">Telefon:</label>
-        <input type="text" name="telefon"><br><br>
-    </fieldset>
-    <fieldset>
-        <legend>Verleih Daten</legend>
-        <label for="raten">Anzahl Raten:</label>
-        <input type="text" name="raten"><br><br>
-        
-        <label for="creditPackage">Kredit Paket:</label>
-        <input type="text" name="creditPackage"><br><br>
-    </fieldset> -->
 
 <style>
+*{
+    font-family: Arial;
+}
 table {
     width:100%;
 }
 table, th, td {
-border: 1px solid black;
-border-collapse: collapse;
+    border: 1px solid black;
+    border-collapse: collapse;
 }
 th, td {
-padding: 15px;
-text-align: left;
+    padding: 15px;
+    text-align: left;
 }
 tr:first-child{
     background-color: #f2f2f2;
 }
+td a, a:visited{
+    color: hsl(197, 0%, 23%);
+    text-decoration: none;
+}
+td a:hover{
+    text-decoration: underline;
+    cursor: pointer;
+}
 button{
-background-color: #4CAF50;
-  border: none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 0px;
-  cursor: pointer;
+    padding: 20px;
+    margin: 4px 0px;
+    color: white;
+    background-color: hsl(197, 100%, 23%);
+    text-decoration: none;
+    box-shadow: 2px 3px 5px gray;
+    transition: 0.3s;
+    border-radius: 8px;
+    border: none;
+}
+button:hover{
+    background-color: hsl(197, 100%, 33%);
 }
 </style>
 
-<h1>Kreditverleihe</h1>
+    <h1>Verleihdaten</h1>
 
-<?php
-echo "
-<table>
-<tr> 
-    <th>PersonID</th> 
-    <th>Email</th> 
-    <th>Name</th> 
-    <th>Telefon</th>
-    <th>Abgeschlossen</th>
-    <th>Bearbeiten</th> 
-</tr>";
-?>
+    <table>
+        
+        <tr> 
+            <th>Name</th> 
+            <th>Email</th> 
+            <th>Telefon</th>
+            <th>Abgeschlossen</th>
+            <th>Bearbeiten</th> 
+        </tr>
 
-<?php foreach ($credits as $credit): ?>
-<tr>
-    <td><?= $credit['PersonID'] ?> </td>
-    <td><?= $credit['Email'] ?></td>
-    <td><?= $credit['Name'] ?></td>
-    <td><?= $credit['Telefon'] ?></td>
-    <td><input type="checkbox"/></td>
-    <td><a href="update?id=<?= $credit['PersonID'] ?>">bearbeiten</a></td>
-    </tr>
-    <?php endforeach; ?>
-</table>
+        <?php foreach ($credits as $credit): ?>
+            <tr>
+                <td><?= $credit['Name'] ?></td>
+                <td><?= $credit['Email'] ?></td>
+                <td><?= $credit['Telefon'] ?></td>
+                <td><input type="checkbox"/></td>
+                <td><a href="update?id=<?= $credit['PersonID'] ?>">Verleih bearbeiten</a></td>
+            </tr>
+        <?php endforeach; ?>
 
-<a href="create"><button>Verleih hinzufügen</button></a>
+    </table>
 
-    
-         
-   
+    <a href="create"><button>Verleih hinzufügen</button></a>
+
 </body>
 </html>
