0e46a7384bfee3aea81169e31ae3299ee1f5167b
LehreInfOdermatt <Matthias_odermatt@sluz.ch>
2021-04-28 14:48:43 +0200

    Clientside Validierung

--- app/Views/createCredit.view.php
+++ app/Views/createCredit.view.php
@@ -1,33 +1,96 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <title>Verleih Erfassung</title>
 </head>
+
 <body>
-<form action="create" method="post">
+    <form action="create" method="post">
         <fieldset>
             <legend>Personal Daten</legend>
 
             <label for="Name">Name:</label>
-            <input type="text" name="name" require><br><br>
+            <input type="text" name="name" id="name" require><br><br>
 
             <label for="email">Email:</label>
-            <input type="text" name="email" require><br><br>
+            <input type="email" name="email" id="email" require><br><br>
 
             <label for="telefon">Telefon:</label>
-            <input type="text" name="telefon"><br><br>
+            <input type="text" name="telefon" id="telefon"><br><br>
         </fieldset>
         <fieldset>
             <legend>Verleih Daten</legend>
             <label for="raten">Anzahl Raten:</label>
-            <input type="text" name="raten" require><br><br>
+            <input type="text" name="raten" id="raten" require><br><br>
 
             <label for="creditPackage">Kredit Paket:</label>
-            <input type="text" name="creditPackage" require>
+            <input type="text" name="creditPackage" id="creditPackage" require>
         </fieldset>
         <button type="submit" name="form-submit">Kreditverleih erfassen</button>
-
     </form>
+    <p id="errorList"></p>
+    <script>
+        // Clientside Validierung
+        var creditPackages = "";
+
+        window.addEventListener("load", function() {
+
+            document.querySelector('form').addEventListener('submit', function(evt) {
+
+                var errors = false;
+                var warnings = document.querySelectorAll(".warning");
+                if (warnings != null) {
+                    warnings.forEach(element => {
+                        element.remove();
+                    });
+                }
+
+
+                if (document.querySelector('#name') != null) {
+                    if (document.querySelector('#name').value.trim() === '') {
+                        document.querySelector('#name').insertAdjacentHTML("afterend", "<label class=\"warning\"> Bitte gib einen Namen ein</label>");
+                        errors = true;
+                    }
+                }
+
+                if (document.querySelector('#email') != null) {
+                    if (document.querySelector('#email').value.trim() === '' || !document.querySelector('#email').value.trim().includes("@")) {
+                        document.querySelector('#email').insertAdjacentHTML("afterend", "<label class=\"warning\"> Bitte gib eine gültige Email ein.</label>");
+                        errors = true;
+                    }
+                }
+
+                if (document.querySelector('#telefon') != null) {
+                    if (document.querySelector('#telefon').value.trim() === '') {
+                        document.querySelector('#telefon').insertAdjacentHTML("afterend", "<label class=\"warning\"> Bitte gib eine gültige Telefonnummer ein.</label>");
+                        errors = true;
+                    }
+                }
+
+                if (document.querySelector('#raten') != null) {
+                    if (document.querySelector('#raten').value.trim() === '') {
+                        document.querySelector('#raten').insertAdjacentHTML("afterend", "<label class=\"warning\"> Bitte gib eine gültige Rate ein.</label>");
+                        errors = true;
+                    }
+                }
+
+                if (document.querySelector('#creditPackage') != null) {
+                    if (document.querySelector('#creditPackage').value.trim() === '') {
+                        document.querySelector('#creditPackage').insertAdjacentHTML("afterend", "<label class=\"warning\"> Bitte gib ein gültiges Kredit Paket ein.</label>");
+                        errors = true;
+                    }
+                }
+
+                if (errors) {
+                    evt.preventDefault();
+                }
+
+            });
+        });
+    </script>
+    <script src="../../public/js/clientSideValidation"></script>
 </body>
+
 </html>
